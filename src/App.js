0b40f31a24b4bba06c81f6c1ebf065283d8daff0
Makarov Danil <ninjaheister@gmail.com>
2020-12-02 05:33:40 +0100

    main menu/ login/ register screens without styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Input from './UI/Input/Input'
 import './index.css'
-
+import Register from './screens/Register/Register'
+import Login from './screens/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Input />
+      <Login />
     </div>
   );
 }
