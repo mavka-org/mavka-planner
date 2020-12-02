e9aa93811404ad9820feb1cf943d30bdfb958796
Artem Laptiev <arlaptiev@gmail.com>
2020-12-02 02:45:53 +0200

    program-changes

--- models/program/program.js
+++ models/program/program.js
@@ -1,11 +1,22 @@
 class Program {
-  constructor(subject="math", ) {
-    this.subject = subject;
-    this.year = None;
+
+  constructor(json) {
+    this.subject = "math";
+    this.modules = []
+    this.chapters = []
     this.topics = []
+
+
   }
 
-  load(json) {
-      this.name =
-  }
 }
+
+let json = {
+    "subject": "math",
+    "year": 2020
+}
+
+console.log("hui");
+
+let w = new Program(json);
+console.log(w.json());
