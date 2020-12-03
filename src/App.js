ca95a81dc9cfa5a8c7103f60fc9f8df070e7ce85
Artem <arlaptiev@gmail.com>
2020-12-03 18:39:32 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './index.css'
-import Login from './screens/Login/Login';
-import Planner from  './models/planner/planner'
+import Test from './screens/test/test'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Test />
+        </div>
+      </ThemeProvider>
   );
 }
 
