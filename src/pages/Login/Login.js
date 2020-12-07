20fd4966901b3c4bf6633952e02836d5af5719f9
SevkavTV <seva.archakov@gmail.com>
2020-12-07 03:59:48 +0200

    auth beginning

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import LoginUI from './LoginUI'
+import Firebase from '../../services/firebase'
 
 class Login extends React.Component{
 
+    handleTelegramResponse = (response) => {
+        var email = response.id + '@mavka.org'
+        var password = response.id
+        console.log(email, password)
+    };
+
     render(){
         return(
-            <LoginUI />
+            <LoginUI handleTelegramResponse={this.handleTelegramResponse}/>
         )
     }
 }
