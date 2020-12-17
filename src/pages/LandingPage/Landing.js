51729d0a84e146cf26cab5513fab74b95d926cf0
SevkavTV <seva.archakov@gmail.com>
2020-12-18 01:35:09 +0200

    okay

--- src/pages/LandingPage/Landing.js
+++ src/pages/LandingPage/Landing.js
@@ -91,7 +91,7 @@ const Landing = (props) => {
 
 
         <Box styles={{ width: '80%', maxWidth: '300px' }} display="flex" justifyContent="center" p={4}>
-          <img width="100%" src={Demo} alt="mavka" />
+          <img width="100%" src={Demo} alt="mavka" styles={{ alignSelf: "flex-start" }}/>
         </Box>
 
 
